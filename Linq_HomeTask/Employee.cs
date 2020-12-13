1ad5fee40a99333ecea82608c901d807d3d73082
Саня FPV <andrei.cazanov2017@gmail.com>
2020-12-13 13:55:46 +0300

    Создал класс Employee

---
+++ Linq_HomeTask/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq_HomeTask
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int DepId { get; set; }
+    }
+}
---
+++ Linq_HomeTask/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Linq_HomeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Finish ->
+            Console.WriteLine("\n> Программа завершена (0_0)");
+        }
+    }
+}
